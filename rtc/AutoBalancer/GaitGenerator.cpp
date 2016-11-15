6cb7a447e766dcab28f4d9e32e5ee5df427d235e
YutaKojio <kojio@jsk.imi.i.u-tokyo.ac.jp>
2016-11-15 21:25:42 +0900

    [Stabilizer.cpp] fix nan in capture point

--- rtc/AutoBalancer/GaitGenerator.cpp
+++ rtc/AutoBalancer/GaitGenerator.cpp
@@ -614,7 +614,7 @@ namespace rats
         d_footstep(2) = 0.0;
         // overwrite footsteps
         if (lcg.get_lcg_count() <= static_cast<size_t>(footstep_nodes_list[lcg.get_footstep_index()][0].step_time/dt * 1.0) - 1 &&
-            lcg.get_lcg_count() >= static_cast<size_t>(footstep_nodes_list[lcg.get_footstep_index()][0].step_time/dt * (default_double_support_ratio_after * margin_time_ratio)) - 1 &&
+            lcg.get_lcg_count() >= static_cast<size_t>(footstep_nodes_list[lcg.get_footstep_index()][0].step_time/dt * (default_double_support_ratio_after + margin_time_ratio)) - 1 &&
             !(lcg.get_lcg_count() <= static_cast<size_t>(footstep_nodes_list[lcg.get_footstep_index()][0].step_time/dt * 0.5) - 1 && contact_states[0] && contact_states[1]) &&
             is_emergency_walking) {
           // stride limitation check
